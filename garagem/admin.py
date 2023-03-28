cffc0d310d5e128e95732d05fdaaa9fd7fcc3881
horodeski <geovana.horodeski06@gmail.com>
2023-03-28 16:35:42 -0300

    Criação da model acessório

--- garagem/admin.py
+++ garagem/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Marca, Categoria
+from .models import Marca, Categoria, Acessorio
 
 admin.site.register(Marca)
 admin.site.register(Categoria)
+admin.site.register(Acessorio)
--- garagem/models.py
+++ garagem/models.py
@@ -12,3 +12,10 @@ class Categoria(models.Model):
 
     def __str__(self):
         return self.descricao
+    
+class Acessorio(models.Model):
+    descricao = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.descricao
+    
