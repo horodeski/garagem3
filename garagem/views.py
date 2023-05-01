0f2765e42563bced55bfd93405daa76dec81850d
horodeski <geovana.horodeski06@gmail.com>
2023-05-01 20:53:36 -0300

    Criação API Acessórios

--- garagem/views.py
+++ garagem/views.py
@@ -1,17 +1,30 @@
 from rest_framework.viewsets import ModelViewSet
 
-from garagem.models import Marca, Categoria, Cor
-from garagem.serializers import MarcaSerializer, CategoriaSerializer, CorSerializer
+from garagem.models import Marca, Categoria, Cor, Veiculo, Acessorio
+from garagem.serializers import (
+    MarcaSerializer,
+    CategoriaSerializer,
+    CorSerializer,
+    AcessorioSerializer,
+)
 
 
 class MarcaViewSet(ModelViewSet):
     queryset = Marca.objects.all()
     serializer_class = MarcaSerializer
-    
+
+
 class CategoriaViewSet(ModelViewSet):
     queryset = Categoria.objects.all()
     serializer_class = CategoriaSerializer
-    
+
+
 class CorViewSet(ModelViewSet):
     queryset = Cor.objects.all()
     serializer_class = CorSerializer
+
+
+class AcessorioViewSet(ModelViewSet):
+    queryset = Acessorio.objects.all()
+    serializer_class = AcessorioSerializer
+
